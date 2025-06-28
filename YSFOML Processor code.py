91a57636fb32d57470db76f3d616e457da188c9c
Yasir Sultan <141232540+professor-YasirSultan@users.noreply.github.com>
2025-06-28 20:03:27 +0300

    Version 1.0.4 Update YSFOML Processor code.py

--- YSFOML Processor code.py
+++ YSFOML Processor code.py
@@ -1,4 +1,4 @@
-# YSFOML (Yasir Sultan File Operations Markup Language) Processor , Version 1.0.3 
+# YSFOML (Yasir Sultan File Operations Markup Language) Processor , Version 1.0.4
 
 import os
 import datetime
